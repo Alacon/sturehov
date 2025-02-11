c502b9c2753b7c47e8d2c2628e9cb3fb58726038
Pelle <113427627+pelle-minddig@users.noreply.github.com>
2025-02-11 11:34:27 +0100

    Update scheduled-get.ts

--- web/src/trigger/scheduled-get.ts
+++ web/src/trigger/scheduled-get.ts
@@ -1,12 +1,10 @@
 import { schedules } from "@trigger.dev/sdk/v3";
+import { getSchema } from "./get-schema";
 
 export const scheduleGet = schedules.task({
   id: "scheduled-get-task",
-  // Every hour
-  cron: "0 * * * *",
-  // Set an optional maxDuration to prevent tasks from running indefinitely
   maxDuration: 300, // Stop executing after 300 secs (5 mins) of compute
   run: async () => {
-    return "Nothing to see"; //getSchema.trigger();
+    return getSchema.trigger();
   },
 });
